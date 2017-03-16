4eb8d57140ec73bb96a2c47191110d9ed2a28caf
GPavia <guillaume.pavia@gmail.com>
2017-03-16 23:15:38 +0100

    update

--- Guillaume/script.js
+++ Guillaume/script.js
@@ -49,3 +49,12 @@ ob.mouseleave(function() {
 });
 
 });
+
+var audio = document.getElementById('background_audio');
+
+document.getElementById('mute').addEventListener('click', function (e)
+{
+    e = e || window.event;
+    audio.muted = !audio.muted;
+    e.preventDefault();
+}, false);
