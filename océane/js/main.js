9f3c9d5d2659eb851e07ea14a8dc19d466088049
agneskllr <agneskeller@mbp-de-agnes.era.local>
2017-03-17 12:58:38 +0100

    essai maj scrollbar

--- océane/js/main.js
+++ océane/js/main.js
@@ -1,20 +1,24 @@
-$('p').click(function(){
-  var $this = $(this);
-  if(!$this.data('changed')){
-     $this.data('changed', true);
-     $this.html('&nbsp;');
-  }
-}).blur(function(){
-  var $this = $(this);
-  var trimmedText = $this.text().trim();
-  if(trimmedText == "" || trimmedText == "."){
-    $this.text('Try me here ');
-    $this.data('changed', false);
-  }
-})
-.on('blur keyup paste', function() {
+if (screen){
+
+  $('p').click(function(){
     var $this = $(this);
-    if ($this.html().length > 10) {
-        $this.html($this.html().slice(0,10));
+    if(!$this.data('changed')){
+       $this.data('changed', true);
+       $this.html('&nbsp;');
     }
-});
+  }).blur(function(){
+    var $this = $(this);
+    var trimmedText = $this.text().trim();
+    if(trimmedText == "" || trimmedText == "."){
+      $this.text('Try me here ');
+      $this.data('changed', false);
+    }
+  })
+  .on('blur keyup paste', function() {
+      var $this = $(this);
+      if ($this.html().length > 10) {
+          $this.html($this.html().slice(0,10));
+      }
+  });
+
+}
