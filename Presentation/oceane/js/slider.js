eed2986e0bdf5e3fddeb0ce6151769911849ccb3
agneskllr <agneskeller@mbp-de-agnes.era.local>
2017-03-17 12:48:21 +0100

    maj slider

--- Presentation/oceane/js/slider.js
+++ Presentation/oceane/js/slider.js
@@ -13,4 +13,8 @@ $(document).ready(function(){
       var fontSize = $( ".mySlider" ).slider("value");
       $(".container").css('font-size',fontSize);
     });
+    $(".mySlider").on( "slide", function() {
+      var fontSize = $( ".mySlider" ).slider("value");
+      $(".container2").css('font-size',fontSize);
+    });
 });
