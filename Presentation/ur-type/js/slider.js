4281b8587c03b206e3e3b46f58061c818ab48f2c
Furter <loraine.furter@gmail.com>
2017-03-19 23:02:18 +0100

    clean code (plus léger) + fix bugs (liens non existants, print stylesheets)

--- Presentation/ur-type/js/slider.js
+++ Presentation/ur-type/js/slider.js
@@ -13,4 +13,19 @@ $(document).ready(function(){
       var fontSize = $( ".mySlider" ).slider("value");
       $(".container").css('font-size',fontSize);
     });
+    
+    $(".mySlider2").slider({
+      min: 8,
+      max: 200,
+      value: 20,
+      orientation: "horizontal",
+    });
+
+    var fontSize = $( ".mySlider2" ).slider("value");
+    $(".container2").css('font-size',fontSize);
+
+    $(".mySlider2").on( "slide", function() {
+      var fontSize = $( ".mySlider2" ).slider("value");
+      $(".container2").css('font-size',fontSize);
+    });
 });
